80fc96d34abeb90ef030a04ec01b39398500cd8a
Raja-25 <rajarajendra925@gmail.com>
2023-10-14 18:41:24 +0530

    signup css

--- src/components/SignUp/Signup.jsx
+++ src/components/SignUp/Signup.jsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import UrlHelper from "../../UrlHelper"
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import './signup.css';
 const SignUpForm = () => {
-    
+
     const nav = useNavigate();
     const [formData, setFormData] = useState({
         username: '',
@@ -24,7 +25,7 @@ const SignUpForm = () => {
         e.preventDefault();
         console.log(formData);
         UrlHelper
-            .post('/reg', formData,{
+            .post('/reg', formData, {
                 headers: {
                     "Access-Control-Allow-Origin": "*",
                 },
@@ -38,58 +39,72 @@ const SignUpForm = () => {
                     toast.success("signup successful");
                     nav("/login");
                 }
-                // You can perform any additional actions here
             })
             .catch((error) => {
                 console.error('Error signing up:', error);
-                // Handle the error appropriately
             });
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label>First Name:</label>
-                <input
-                    type="text"
-                    name="username"
-                    value={formData.username}
-                    onChange={handleChange}
-                    required
-                />
+        <div className='signupcon'>
+        <div className='container mt-5'>
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <div class="card">
+                        <h5 class="card-header text-center">Register New Account</h5>
+                        <div class="card-body"></div>
+                        <form onSubmit={handleSubmit}>
+                            <div className="form-group mb-4">
+                                <label>First Name:</label>
+                                <input
+                                    className="form-control"
+                                    type="text"
+                                    name="username"
+                                    value={formData.username}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                            <div className="form-group mb-4">
+                                <label>Last Name:</label>
+                                <input
+                                className="form-control"
+                                    type="email"
+                                    name="email"
+                                    value={formData.email}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                            <div className="form-group mb-4">
+                                <label>Email:</label>
+                                <input
+                                className="form-control"
+                                    type="text"
+                                    name="contact"
+                                    value={formData.contact}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                            <div className="form-group mb-4">
+                                <label>Password:</label>
+                                <input
+                                className="form-control"
+                                    type="password"
+                                    name="password"
+                                    value={formData.password}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                            <button type="submit" className="btn btn-danger btn-block">Sign Up</button>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <div>
-                <label>Last Name:</label>
-                <input
-                    type="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    required
-                />
-            </div>
-            <div>
-                <label>Email:</label>
-                <input
-                    type="text"
-                    name="contact"
-                    value={formData.contact}
-                    onChange={handleChange}
-                    required
-                />
-            </div>
-            <div>
-                <label>Password:</label>
-                <input
-                    type="password"
-                    name="password"
-                    value={formData.password}
-                    onChange={handleChange}
-                    required
-                />
-            </div>
-            <button type="submit">Sign Up</button>
-        </form>
+        </div>
+        </div>
     );
 };
 
