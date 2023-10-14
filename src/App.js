9d2744fa7180563dc09d639294f99fe5a93eb581
Raja-25 <rajarajendra925@gmail.com>
2023-10-14 14:45:40 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React,{useEffect,useState} from 'react'
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
+import { Routes,Route} from 'react-router-dom';
+import About from './components/About/About';
+import Home from './components/Home';
+import Footer from './components/Footer';
+import Login from './components/Login/Login';
+import Blog from './components/Blog/Blog';
+import Contact from './components/Contact/Contact';
+import BlogDetails from './components/Blog/BlogDetails';
+import Shop from './components/Shop/Shop';
 function App() {
+  const [isSticky, setIsSticky] = useState(false);
+  const [isDropdownHover, setIsDropdownHover] = useState(false);
+
+  // Sticky Navbar
+  useEffect(() => {
+      window.onscroll = () => {
+          if (window.scrollY > 40) {
+              setIsSticky(true);
+          } else {
+              setIsSticky(false);
+          }
+      };
+  }, []);
+
+  // Dropdown on mouse hover
+  useEffect(() => {
+      const toggleNavbarMethod = () => {
+          if (window.innerWidth > 992) {
+              setIsDropdownHover(true);
+          } else {
+              setIsDropdownHover(false);
+          }
+      };
+
+      toggleNavbarMethod();
+      window.onresize = toggleNavbarMethod;
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar></Navbar>
+
+    <Routes>
+      <Route path='/' element={<Home/>} ></Route> 
+      <Route path='/about' element={<About/>} ></Route> 
+      <Route path='/login' element={<Login/>} ></Route> 
+      <Route path='/blog' element={<Blog/>} ></Route> 
+      <Route path='/contact' element={<Contact/>} ></Route> 
+      <Route path='/blogdetails' element={<BlogDetails/>} ></Route> 
+      <Route path='/shop' element={<Shop/>} ></Route> 
+    </Routes>
+    <Footer></Footer>
+    </>
   );
 }
 
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+import "./Navbar.css"
+export default function Navbar() {
+  return (
+
+    <>
+    <div className='navbarmain'>
+      <nav className="navbar navbar-expand-lg bg-white navbar-light shadow-sm py-3 py-lg-0 px-3 px-lg-0">
+        <NavLink to="/" className="navbar-brand ms-lg-5">
+          <h1 className="m-0 text-uppercase text-dark">
+            <i className="fa-solid fa-paw"></i>&nbsp;Pet Nirvana
+          </h1>
+        </NavLink>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <div className="navbar-nav ms-auto py-0">
+            <NavLink to="/" className="nav-item nav-NavLink active">Home</NavLink>
+            <NavLink to="/Templates/PetGallery" className="nav-item nav-NavLink">Pets</NavLink>
+            <NavLink to="/" className="nav-item nav-NavLink">Stories</NavLink>
+            <NavLink to="/Templates/shop" className="nav-item nav-NavLink">Shop</NavLink>
+
+            <div className="nav-item dropdown">
+              <NavLink to="/" className="nav-NavLink dropdown-toggle" data-bs-toggle="dropdown">Account</NavLink>
+              <div className="dropdown-menu m-0">
+                <NavLink to="/login" className="dropdown-item">Sign in</NavLink>
+                <NavLink to="/team" className="dropdown-item">Sign up</NavLink>
+              </div>
+            </div>
+            <NavLink to="/Templates/contact" className="nav-item nav-NavLink nav-contact bg-success text-white px-5 ms-lg-5">Contact <i className="bi bi-arrow-right"></i></NavLink>
+          </div>
+        </div>
+      </nav>
+      </div>
+</>
+  )
+}
